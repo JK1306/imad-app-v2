43ef2c80056a2aba481fabcc13c30acc37d9f4b2
JK1306 <jaikishore1997@gmail.com>
2017-06-30 13:03:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/des.html', function (req, res) {
+app.get('/ui/des.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'des.html'));
 });
 
